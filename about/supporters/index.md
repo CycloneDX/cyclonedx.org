d4cb7d2f2f73899caa18169a24417bd3d0911561
Steve Springett <steve@springett.us>
2021-09-14 00:28:10 -0500

    Added supporters footer and dedicated supporters page

---
+++ about/supporters/index.md
@@ -0,0 +1,37 @@
+---
+# Page settings
+layout: supporters
+keywords: application security, software security, software bill of material, SBOM, BOM, open source, supply chain, specification, spdx, license, package url, purl, cpe
+comments: false
+banner: false
+
+# Hero section
+title: CycloneDX Supporters
+window_title: CycloneDX Supporters
+description: CycloneDX Supporters
+
+# Micro navigation
+micro_nav: false
+
+# Page navigation
+    
+---
+
+# Supporters
+
+&nbsp;<!-- without this hack, the dropdown menu has issues due to h1 and h2 happening right after each other -->
+
+## CycloneDX Supporters
+
+<div class="logo-cards" id="adopters-supporters">  
+</div>
+
+## Vendor Support
+
+<div class="logo-cards" id="adopters-organizations">  
+</div>
+
+## Project Support
+
+<div class="logo-cards" id="adopters-projects">  
+</div>
---
+++ theme/assets/js/supporters.js
@@ -0,0 +1,55 @@
+function populateCards() {
+    let supporters = document.getElementById("adopters-supporters");
+    let organizations = document.getElementById("adopters-organizations");
+    let projects = document.getElementById("adopters-projects");
+    let supportersHtml = "";
+    let organizationsHtml = "";
+    let projectsHtml = "";
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let members = JSON.parse(this.responseText);
+            // Sort alphabetically by organization
+            members.sort((a, b) => (a.organization > b.organization) ? 1 : -1);
+            for (let i = 0; i < members.length; i++) {
+                let member = members[i];
+                let template = generateTemplate(member);
+                for (let aa = 0; aa < member.categories.length; aa++) {
+                    if (member.categories[aa] === "supporters") {
+                        supportersHtml += template;
+                    } else if (member.categories[aa] === "organizations") {
+                        organizationsHtml += template;
+                    } else if (member.categories[aa] === "projects") {
+                        projectsHtml += template;
+                    }
+                }
+            }
+            supporters.insertAdjacentHTML('beforeend', supportersHtml);
+            organizations.insertAdjacentHTML('beforeend', organizationsHtml);
+            projects.insertAdjacentHTML('beforeend', projectsHtml);
+
+        }
+    };
+    xmlhttp.open("GET", "/about/supporters/supporters.json", true);
+    xmlhttp.send();
+}
+
+function generateTemplate(entity) {
+    if (entity.logo.startsWith('https://'))
+    {
+        var logoUrl = entity.logo;
+    }
+    else
+    {
+        var logoUrl = "/theme/assets/images/logos/" + entity.logo;
+    }
+    return `
+    <div class="logo-card-container">
+      <div class="logo-card">
+        <div class="logo-card-body"><a href="${entity.homepage}"><img class="adopter" src="${logoUrl}" alt="${entity.organization}" border="0"></a></div>
+      </div>
+    </div>  
+    `;
+}
+
+populateCards();
